a64022610adcad5cf1b2bb9cb8a44816d0ef0c64
Johannes Pötzsch <johannes.poetzsch@student.hpi.uni-potsdam.de>
2021-11-13 16:23:57 +0100

    Solved Issue #4

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe "Author Model", type: :model do
+    it "should have first name, last name, and homepage" do
+        author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+        expect(author.first_name).to eq("Alan")
+        expect(author.last_name).to eq("Turing")
+        expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+    end
+end

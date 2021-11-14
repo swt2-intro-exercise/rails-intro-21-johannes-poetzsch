e13013d533950ced8927ca00e48ec7deb1326855
Johannes Pötzsch <johannes.poetzsch@student.hpi.uni-potsdam.de>
2021-11-15 00:30:48 +0100

    Solved Issue #15

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -17,4 +17,10 @@ describe "Author", type: :model do
         @author = Author.new(first_name: "Alan", homepage: "http://wikipedia.org/Alan_Turing")
         expect(@author).to_not be_valid
     end
+
+    
+    it "should have and belong to many papers" do
+        @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+        expect(@author.papers).to be_empty
+    end
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -17,4 +17,9 @@ RSpec.describe Paper, type: :model do
     @paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: "not a number")
     expect(@paper).to_not be_valid
   end
+
+  it "should have and belong to many authors" do
+    @paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
+    expect(@paper.authors).to be_empty
+  end
 end

a3bff71c3fcdde3e8b0984f8c9a0af327ebe909c
Johannes Pötzsch <johannes.poetzsch@student.hpi.uni-potsdam.de>
2021-11-14 18:01:00 +0100

    Solves Issue #9

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,7 @@
 class Author < ApplicationRecord
+    validates :last_name,   presence: true,
+                            length: { minimum: 1 }
+
     def name
         first_name.dup << " " << last_name
     end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,15 +1,20 @@
 require 'rails_helper'
 
-describe "Author Model", type: :model do
+describe "Author", type: :model do
     it "should have first name, last name, and homepage" do
-        author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
-        expect(author.first_name).to eq("Alan")
-        expect(author.last_name).to eq("Turing")
-        expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+        @author = Author.new(first_name: "Alan", last_name: "Turing", homepage: "http://wikipedia.org/Alan_Turing")
+        expect(@author.first_name).to eq("Alan")
+        expect(@author.last_name).to eq("Turing")
+        expect(@author.homepage).to eq("http://wikipedia.org/Alan_Turing")
     end
 
     it "#name should return the full name" do
-        a = Author.new(first_name: "Alan", last_name: "Turing")
-        expect(a.name).to eq("Alan Turing")
+        @author = Author.new(first_name: "Alan", last_name: "Turing")
+        expect(@author.name).to eq("Alan Turing")
+    end
+
+    it "should not validate without last name" do
+        @author = Author.new(first_name: "Alan", homepage: "http://wikipedia.org/Alan_Turing")
+        expect(@author).to_not be_valid
     end
 end

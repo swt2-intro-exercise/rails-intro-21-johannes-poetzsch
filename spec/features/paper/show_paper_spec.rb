25c9a88d2fea269e68859278f0df10432ac790de
Johannes Pötzsch <johannes.poetzsch@student.hpi.uni-potsdam.de>
2021-11-15 23:42:46 +0100

    Solved Issue #16

---
+++ spec/features/paper/show_paper_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "Paper details page", type: :feature do
+    it "should exist at 'paper_path(@paper)' and render without error" do
+        @paper = FactoryBot.create :paper
+        visit paper_path(@paper)
+    end
+
+    it "should display paper details" do
+        @paper = FactoryBot.create :paper
+        visit paper_path(@paper)
+        expect(page).to have_text @paper.title
+        expect(page).to have_text @paper.venue
+        expect(page).to have_text @paper.year
+    end
+
+    it "should list the authors" do
+        @paper = FactoryBot.create :paper
+        visit paper_path(@paper)
+        @paper.authors.each do |author|
+            expect(page).to have_text author.name
+        end
+    end
+end
